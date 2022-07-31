540f2323458a04c91005823bc51ff41df50711e6
Radek Bednarik <radek.bednarik@tesena.com>
2022-07-31 13:43:47 +0200

    better

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
 import fetch from "node-fetch";
 
 const getCatFact = async ({ url = "https://catfact.ninja/fact" }: { url?: string }) => {
-    try {
-        const response = await fetch(url);
-        const body = await response.json()
-        if (response.ok) {
-            return body;
-        }
-        console.error(`GET ${url} return error: ${JSON.stringify(body)}`);
-        process.exit(1);
-    } catch (err) {
-        console.error(`Error when trying to GET ${url}: ${err}`);
-        process.exit(1)
+  try {
+    const response = await fetch(url);
+    const body = await response.json();
+    if (response.ok) {
+      return body;
     }
-}
+    console.error(`GET ${url} return error: ${JSON.stringify(body)}`);
+    return { error: JSON.stringify(body) };
+  } catch (err) {
+    console.error(`Error when trying to GET ${url}: ${err}`);
+    return { error: JSON.stringify(err) };
+  }
+};
 
 const gimmeFacts = async ({ howMany = 10 }: { howMany?: number }) => {
-    const iterable = Array(howMany);
-    const promisesContainer = [];
-    for (const _ of iterable) {
-        promisesContainer.push(getCatFact({}));
-    }
-    return await Promise.allSettled(promisesContainer);
-}
+  const promisesContainer = [];
+
+  for (let i = 0; i < howMany; i++) {
+    promisesContainer.push(getCatFact({}));
+  }
+
+  return await Promise.allSettled(promisesContainer);
+};
 
 const main = async () => {
-    const facts = await gimmeFacts({});
-    console.log(JSON.stringify(facts, null, 2));
-}
+  const facts = await gimmeFacts({});
+  console.log(JSON.stringify(facts, null, 2));
+};
 
 main();
